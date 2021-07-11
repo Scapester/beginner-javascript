3e4125a0ee6cee10f4830876b60cc65d241fbb0b
Scapester <84982335+Scapester@users.noreply.github.com>
2021-07-11 19:43:35 +0200

    Scoping Exercises

---
+++ playground/scope.js
@@ -0,0 +1,52 @@
+/* eslint-disable */ 
+
+// console.log('It works!');
+
+// const first = 'Scott';
+// let second = 'Gardner';
+// var age = 49;
+
+// function sayHi() {
+//   console.log(`Hi!`);
+// }
+
+// const age = 100;
+
+// function go() {
+//   const age = 200;
+//   const hair = 'blonde';
+//   console.log(hair);
+//   console.log(age);
+// }
+
+// console.log(age);
+// go();
+// console.log(hair);
+
+// function isCool(name){
+    // let cool; 
+
+//     if (name === 'Scott') {
+//        var cool = true;
+//     }
+
+//     console.log(cool);
+//     return cool; 
+// }
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+const dog = 'Sunny';
+
+function logDog(){
+    console.log(dog);
+}
+
+function go(){
+    const dog = 'Snickers';
+    logDog();
+}
+
+go();
